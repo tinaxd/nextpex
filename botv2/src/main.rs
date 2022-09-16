de8805efe40914497a034da43a8b868874cda751
tinaxd <31384595+tinaxd@users.noreply.github.com>
2022-09-16 22:22:39 +0900

    role change

--- botv2/src/main.rs
+++ botv2/src/main.rs
@@ -1,4 +1,5 @@
 extern crate redis;
+extern crate reqwest;
 extern crate serenity;
 extern crate tokio;
 
@@ -8,9 +9,11 @@ use std::env;
 use std::sync::Arc;
 
 use serenity::async_trait;
+use serenity::framework::StandardFramework;
+use serenity::model;
 use serenity::model::channel::Message;
 use serenity::model::gateway::Ready;
-use serenity::model::prelude::{Emoji, Guild, GuildChannel, Presence};
+use serenity::model::prelude::{Activity, ActivityType, Emoji, Guild, GuildChannel, Presence};
 use serenity::prelude::*;
 
 use redis::AsyncCommands;
@@ -34,34 +37,52 @@ async fn get_players_from_redis(conn: &mut redis::aio::Connection) -> RedisResul
     }
 }
 
-async fn get_guild_ids(conn: &mut redis::aio::Connection) -> RedisResult<Vec<String>> {
+async fn get_guild_ids(
+    conn: &mut redis::aio::Connection,
+) -> RedisResult<Vec<serenity::model::id::GuildId>> {
     match conn.smembers("guild_ids").await {
         Err(e) => Err(e),
         Ok(guild_ids) => {
-            let mut guild_ids: Vec<String> = guild_ids;
+            let mut guild_ids: Vec<u64> = guild_ids;
             guild_ids.sort();
-            Ok(guild_ids)
+            Ok(guild_ids
+                .iter()
+                .map(|x| serenity::model::id::GuildId(*x))
+                .collect())
         }
     }
 }
 
 async fn get_target_channel(
     conn: &mut redis::aio::Connection,
-    guild_id: &str,
-) -> RedisResult<Option<i32>> {
-    match conn.get(format!("mc_channel:{}", guild_id)).await {
+    guild_id: &serenity::model::id::GuildId,
+) -> RedisResult<Option<serenity::model::id::ChannelId>> {
+    match conn.get(format!("mc_channel:{}", guild_id.0)).await {
         Err(e) => Err(e),
         Ok(channel_id) => match channel_id {
-            Some(channel_id) => Ok(Some(channel_id)),
+            Some(channel_id) => Ok(Some(serenity::model::id::ChannelId(channel_id))),
             None => Ok(None),
         },
     }
 }
 
+async fn set_target_channel(
+    conn: &mut redis::aio::Connection,
+    guild_id: &serenity::model::id::GuildId,
+    channel_id: &serenity::model::id::ChannelId,
+) -> RedisResult<()> {
+    conn.set(format!("mc_channel:{}", guild_id.0), channel_id.0)
+        .await
+}
+
 fn build_message(players: &[String]) -> String {
-    let msg = "サーバーに参加中のプレイヤー:\n";
-    let pls = players.join("\n");
-    format!("{}{}", msg, pls)
+    if players.len() > 0 {
+        let msg = "サーバーに参加中のプレイヤー:\n";
+        let pls = players.join("\n");
+        format!("{}{}", msg, pls)
+    } else {
+        "サーバーに参加中のプレイヤーはいません".to_string()
+    }
 }
 
 async fn process_event(
@@ -90,11 +111,10 @@ async fn process_event(
         } {
             let guild_ids = get_guild_ids(conn).await.map_err(|e| e.to_string())?;
             for guild_id in guild_ids {
-                match get_target_channel(conn, guild_id.as_str()).await {
+                match get_target_channel(conn, &guild_id).await {
                     Err(e) => return Err(e.to_string()),
                     Ok(channel_id) => match channel_id {
                         Some(channel_id) => {
-                            let channel_id = serenity::model::id::ChannelId(channel_id as u64);
                             if let Err(e) = channel_id.say(&ctx.http, text.as_str()).await {
                                 return Err(e.to_string());
                             }
@@ -130,8 +150,67 @@ async fn redis_loop(mut conn: redis::aio::Connection, ctx: Context) {
     }
 }
 
+async fn find_apexable_role(
+    ctx: &Context,
+    guild: &model::id::GuildId,
+) -> serenity::Result<Option<model::guild::Role>> {
+    let roles = guild.roles(&ctx.http).await?;
+    for (_, role) in roles {
+        if role.name == "APEXable" {
+            return Ok(Some(role));
+        }
+    }
+    Ok(None)
+}
+
+async fn apex_role_change(
+    ctx: &Context,
+    member: &mut model::guild::Member,
+    on: bool,
+) -> serenity::Result<()> {
+    match find_apexable_role(ctx, &member.guild_id).await? {
+        None => Ok(()),
+        Some(role) => {
+            if on {
+                member.add_role(&ctx.http, role.id).await?;
+            } else {
+                member.remove_role(&ctx.http, role.id).await?;
+            }
+            Ok(())
+        }
+    }
+}
+
+async fn send_game_notification(
+    member: &mut serenity::model::guild::Member,
+    ctx: &Context,
+    event: &GameEvent,
+) -> serenity::Result<()> {
+    let guild = &member.guild_id;
+    let chan = find_channel(ctx, guild, "self-apexability").await?;
+    if let Some(chan) = chan {
+        let (tail, game, on) = match event {
+            GameEvent::Start(game) => (format!("{} を始めました！", game), game, true),
+            GameEvent::End(game) => (format!("{} をやめました！", game), game, false),
+        };
+        let content = format!("{} が {}", member.display_name(), tail);
+        chan.say(&ctx.http, content).await?;
+
+        if game == "Apex Legends" {
+            apex_role_change(ctx, member, on).await?;
+        }
+        todo!("send to nextpex");
+    }
+
+    Ok(())
+}
+
 impl Handler {
-    async fn send_apexability_msg(&self, ctx: &Context, guild: &Guild) -> serenity::Result<()> {
+    async fn send_apexability_msg(
+        &self,
+        ctx: &Context,
+        guild: &serenity::model::id::GuildId,
+    ) -> serenity::Result<()> {
         let chan = find_channel(&ctx, guild, SELF_APEX_CHAN).await?;
         if let Some(chan) = chan {
             let content = "Apex Legends を始めたら :apex: リアクションをつけてください。やめたらリアクションを外してください。過去のメッセージにリアクションをつけても反応しません。Discord のステータスメッセージを公開している人はリアクションをつける必要はありません。";
@@ -139,11 +218,14 @@ impl Handler {
             let emoji = find_emoji(&ctx, guild, "apex").await?;
             if let Some(emoji) = emoji {
                 let msg = chan.say(&ctx.http, content).await?;
-                self.set_watching_msg(guild.id, msg.id);
+                self.set_watching_msg(guild.clone(), msg.id).await;
                 msg.react(&ctx.http, emoji).await?;
                 Ok(())
             } else {
-                println!(":apex: emoji not found in {}", guild.name);
+                let guild_name = guild
+                    .name(&ctx.cache)
+                    .unwrap_or("<unknown guild>".to_string());
+                println!(":apex: emoji not found in {}", guild_name);
                 Ok(())
             }
         } else {
@@ -172,11 +254,13 @@ impl EventHandler for Handler {
     // events can be dispatched simultaneously.
     async fn message(&self, ctx: Context, msg: Message) {
         let content = &msg.content;
+        // println!("got message: {}", content);
         let prefix = "!minecraft ";
         if !content.starts_with(prefix) {
             return;
         }
-        let content = &content[(prefix.len() + 1)..];
+        let content = &content[prefix.len()..];
+        // println!("content: {} ", content);
         if content == "players" {
             // Sending a message can fail, due to a network error, an
             // authentication error, or lack of permissions to post in the
@@ -196,6 +280,30 @@ impl EventHandler for Handler {
                     }
                 }
                 Err(e) => println!("RedisError: {:?}", e),
+            }
+        } else if content == "set" {
+            match self.redis.get_async_connection().await {
+                Err(e) => println!("RedisError: {:?}", e),
+                Ok(mut conn) => {
+                    if let Some(guild_id) = msg.guild_id {
+                        let channel_id = msg.channel_id;
+                        if let Err(e) = set_target_channel(&mut conn, &guild_id, &channel_id).await
+                        {
+                            println!("Error set_target_channel: {:?}", e);
+                            return;
+                        }
+                        if let Err(why) = msg
+                            .channel_id
+                            .say(
+                                &ctx.http,
+                                "このチャンネルをMinecraftログインの通知先に設定しました",
+                            )
+                            .await
+                        {
+                            println!("Error sending message: {:?}", why);
+                        }
+                    }
+                }
             }
         }
     }
@@ -220,14 +328,184 @@ impl EventHandler for Handler {
 
     // async fn guild_member_update(&self, _ctx: Context, _new: GuildMemberUpdateEvent) {}
 
-    async fn presence_update(&self, _ctx: Context, _new_data: Presence) {
-        println!("presence_update: {:?}", _new_data);
+    async fn presence_update(&self, ctx: Context, new_data: Presence) {
+        // _new_data.user.id;
+        // println!("presence_update: {:?}", _new_data);
+        match self.redis.get_async_connection().await {
+            Err(e) => println!("RedisError: {:?}", e),
+            Ok(mut conn) => match detect_game_from_presence(&mut conn, &new_data).await {
+                Err(e) => println!("Error: {:?}", e),
+                Ok(game) => match game {
+                    None => {}
+                    Some(game) => {
+                        let user = &new_data.user;
+                        let guild_id = new_data.guild_id;
+                        match guild_id {
+                            None => {}
+                            Some(guild_id) => match guild_id.member(&ctx, user.id).await {
+                                Ok(mut member) => {
+                                    if let Err(e) =
+                                        send_game_notification(&mut member, &ctx, &game).await
+                                    {
+                                        println!("Error: {:?}", e);
+                                    }
+                                }
+                                Err(e) => println!("Error: {:?}", e),
+                            },
+                        }
+                    }
+                },
+            },
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+enum GameEvent {
+    Start(String),
+    End(String),
+}
+
+async fn detect_game_from_presence(
+    conn: &mut redis::aio::Connection,
+    new_presence: &Presence,
+) -> RedisResult<Option<GameEvent>> {
+    let user_id = &new_presence.user.id;
+
+    let old_act = get_last_presence_of_user(conn, user_id)
+        .await?
+        .map(|a| vec![a]);
+
+    let new_act = new_presence
+        .activities
+        .iter()
+        .map(|a| ActivityCore {
+            name: a.name.clone(),
+            kind: a.kind,
+        })
+        .collect::<Vec<ActivityCore>>();
+
+    let started = is_game_start(&old_act, &new_act);
+    let stopped = is_game_stop(&old_act, &new_act);
+    let status = (started, stopped);
+    let result = Ok(match status {
+        (None, None) => {
+            // not related to game
+            None
+        }
+        (Some(game), None) => {
+            // game start
+            Some(GameEvent::Start(game))
+        }
+        (None, Some(game)) => {
+            // game stop
+            Some(GameEvent::End(game))
+        }
+        (Some(_), Some(_)) => {
+            // really reachable??
+            println!("error???: started and stopped");
+            None
+        }
+    });
+
+    // update last presence
+    let new_act_core = new_act
+        .iter()
+        .find(|a| a.kind == ActivityType::Playing)
+        .map(|e| e.clone());
+    set_presence_of_user(conn, user_id, &new_act_core).await?;
+    result
+}
+
+#[derive(Debug, Clone)]
+struct ActivityCore {
+    pub kind: ActivityType,
+    pub name: String,
+}
+
+async fn get_last_presence_of_user(
+    conn: &mut redis::aio::Connection,
+    user_id: &serenity::model::id::UserId,
+) -> RedisResult<Option<ActivityCore>> {
+    let act = conn.get(format!("activity:{}", user_id)).await?;
+    match act {
+        None => Ok(None),
+        Some(act) => {
+            let act: String = act;
+            Ok(Some(ActivityCore {
+                kind: ActivityType::Playing,
+                name: act,
+            }))
+        }
+    }
+}
+
+async fn set_presence_of_user(
+    conn: &mut redis::aio::Connection,
+    user_id: &serenity::model::id::UserId,
+    act: &Option<ActivityCore>,
+) -> RedisResult<()> {
+    match act {
+        Some(act) => {
+            conn.set(format!("activity:{}", user_id), act.name.clone())
+                .await?
+        }
+        None => conn.del(format!("activity:{}", user_id)).await?,
+    }
+    Ok(())
+}
+
+fn is_game_start(old_act: &Option<Vec<ActivityCore>>, new_act: &[ActivityCore]) -> Option<String> {
+    // ゲームをプレイしていなかったことを確認
+    if let Some(old_act) = old_act {
+        for act in old_act {
+            match act.kind {
+                ActivityType::Playing => return None,
+                _ => continue,
+            }
+        }
+    }
+
+    // 今ゲームをプレイしていることを確認
+    for act in new_act {
+        match act.kind {
+            ActivityType::Playing => {
+                return Some(act.name.clone());
+            }
+            _ => continue,
+        }
+    }
+    None
+}
+
+fn is_game_stop(old_act: &Option<Vec<ActivityCore>>, new_act: &[ActivityCore]) -> Option<String> {
+    // ゲームをプレイしていたことを確認
+    let game_name = old_act
+        .clone()
+        .map(|old_act| {
+            old_act
+                .iter()
+                .find(|act| act.kind == ActivityType::Playing)
+                .map(|act| act.name.clone())
+        })
+        .flatten();
+
+    match game_name {
+        None => return None,
+        Some(game_name) => {
+            // 今ゲームをしていないことを確認
+            if new_act.iter().all(|act| act.kind != ActivityType::Playing) {
+                return Some(game_name);
+            } else {
+                return None;
+            }
+        }
     }
 }
 
 async fn find_channel(
     ctx: &Context,
-    guild: &Guild,
+    guild: &serenity::model::id::GuildId,
     chan_name: &str,
 ) -> serenity::Result<Option<GuildChannel>> {
     let channels = guild.channels(&ctx.http).await?;
@@ -243,7 +521,7 @@ const SELF_APEX_CHAN: &'static str = "self-apexability";
 
 async fn find_emoji(
     ctx: &Context,
-    guild: &Guild,
+    guild: &serenity::model::id::GuildId,
     emoji_name: &str,
 ) -> serenity::Result<Option<Emoji>> {
     let emojis = guild.emojis(&ctx.http).await?;
@@ -277,6 +555,7 @@ async fn main() {
             redis: r,
             watching_msg: Arc::new(Mutex::new(RefCell::new(HashMap::new()))),
         })
+        .framework(StandardFramework::new())
         .await
         .expect("Err creating client");
 
